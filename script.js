af8b5e967b89e13ff9c69ea4ef8744e5ae9dc93e
João Estulano <joaomarcelothoud@gmail.com>
2024-10-28 02:22:11 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Interatividade do site, animações ou outras funções podem ser adicionadas aqui
+    console.log("Bem-vindo ao EstuTech!");
+});
