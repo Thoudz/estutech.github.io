b5005448a249edff48b82e52f710847a07d15005
João Estulano <joaomarcelothoud@gmail.com>
2024-11-18 00:34:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     // Adicione interatividade aqui, como animações de scroll ou efeitos em botões
     console.log("Bem-vindo ao EstuTech!");
 });
+document.addEventListener("DOMContentLoaded", () => {
+    const intro = document.getElementById("intro");
+    setTimeout(() => {
+        intro.style.display = "none";
+    }, 3000); // Oculta após 3 segundos
+});
